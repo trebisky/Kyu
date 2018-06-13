b642503fa7a178853d18679b912b4bd331861d3d
Tom Trebisky <tom@mmto.org>
2018-06-12 21:37:39 -0700

    Satisfied with merge of Marco's multicore stuff

--- arm/mmu.c
+++ arm/mmu.c
@@ -41,39 +41,6 @@
 /* Mask for TTBR0 */
 #define MMU_BASE_MASK 0x3fff
 
-#define	PDE_PTSEC	0x00000002 	/* Page Table Entry is 1 MB Section */
-#define	PDE_B		0x00000004      /* Bufferable */
-#define	PDE_C		0x00000008	/* Cacheable */
-#define	PDE_XN		0x00000010      /* Execute Never */
-#define	PDE_DOM		0x000001E0	/* Domain Bits */
-#define	PDE_S		0x00010000	/* Shareable */
-#define	PDE_NG		0x00020000	/* Not Global */
-#define	PDE_NS		0x00080000	/* Non-Secure */
-#define	PDE_BASEADDR	0xFFF00000
-
-/* Access Permissions */
-#define	PDE_AP		0x00008C00	/* Access Permission Bits */
-#define	PDE_AP0		0x00000000
-#define	PDE_AP1		0x00000400
-#define	PDE_AP2		0x00000800
-#define	PDE_AP3		0x00000C00	/* Full Access */
-#define	PDE_AP4		0x00008000
-#define	PDE_AP5		0x00008400
-#define	PDE_AP6		0x00008800
-#define	PDE_AP7		0x00008C00
-
-/* Memory Region Attributes */
-#define	PDE_TEX		0x00007000
-#define	PDE_TEX0	0x00000000
-#define	PDE_TEX1	0x00001000
-#define	PDE_TEX2	0x00002000
-#define	PDE_TEX3	0x00003000
-#define	PDE_TEX4	0x00004000
-#define	PDE_TEX5	0x00005000
-#define	PDE_TEX6	0x00006000
-#define	PDE_TEX7	0x00007000
-
-
 static void
 mmu_display ( unsigned long ttbr )
 {
@@ -170,6 +137,7 @@ mmu_status ( void )
 	    printf ( "I cache enabled\n" );
 }
 
+#ifdef notdef
 /* ARM v7 now has actual barrier instructions, but these work for
  * older ARM (like v5) as well as v7.
  * At some point U-boot used these since it compiled with -march=armv5
@@ -178,6 +146,7 @@ mmu_status ( void )
 #define CP15ISB asm volatile ("mcr     p15, 0, %0, c7, c5, 4" : : "r" (0))
 #define CP15DSB asm volatile ("mcr     p15, 0, %0, c7, c10, 4" : : "r" (0))
 #define CP15DMB asm volatile ("mcr     p15, 0, %0, c7, c10, 5" : : "r" (0))
+#endif
 
 void
 invalidate_tlb ( void )
@@ -190,8 +159,10 @@ invalidate_tlb ( void )
         asm volatile ("mcr p15, 0, %0, c8, c5, 0" : : "r" (0));
 
         /* Barriers so we don't move on until this is complete */
-        CP15DSB;
-        CP15ISB;
+        // CP15DSB;
+        // CP15ISB;
+	asm volatile ( "dsb" );
+	asm volatile ( "isb" );
 }
 
 void
@@ -629,11 +600,43 @@ mmu_debug ( void )
 }
 #endif
 
-/*
-XXX - Xinu follows ...
-*/
+#ifdef XINU_MARCO
+/* For reference - this is how Marco set up the page tables for Xinu */
 
+/* Bogus, so the following will compile, never used */
 static int xinu_page_table[4096];
+
+#define	PDE_PTSEC	0x00000002 	/* Page Table Entry is 1 MB Section */
+#define	PDE_B		0x00000004      /* Bufferable */
+#define	PDE_C		0x00000008	/* Cacheable */
+#define	PDE_XN		0x00000010      /* Execute Never */
+#define	PDE_DOM		0x000001E0	/* Domain Bits */
+#define	PDE_S		0x00010000	/* Shareable */
+#define	PDE_NG		0x00020000	/* Not Global */
+#define	PDE_NS		0x00080000	/* Non-Secure */
+#define	PDE_BASEADDR	0xFFF00000
+
+/* Access Permissions */
+#define	PDE_AP		0x00008C00	/* Access Permission Bits */
+#define	PDE_AP0		0x00000000
+#define	PDE_AP1		0x00000400
+#define	PDE_AP2		0x00000800
+#define	PDE_AP3		0x00000C00	/* Full Access */
+#define	PDE_AP4		0x00008000
+#define	PDE_AP5		0x00008400
+#define	PDE_AP6		0x00008800
+#define	PDE_AP7		0x00008C00
+
+/* Memory Region Attributes */
+#define	PDE_TEX		0x00007000
+#define	PDE_TEX0	0x00000000
+#define	PDE_TEX1	0x00001000
+#define	PDE_TEX2	0x00002000
+#define	PDE_TEX3	0x00003000
+#define	PDE_TEX4	0x00004000
+#define	PDE_TEX5	0x00005000
+#define	PDE_TEX6	0x00006000
+#define	PDE_TEX7	0x00007000
 
 void
 paging_init (void)
@@ -683,54 +686,6 @@ paging_init (void)
 
 	asm volatile ("isb\ndsb\ndmb\n");
 }
-
-/*------------------------------------------------------------------------
- * mmu_set_ttbr  -  Set the Translation Table Base Register
- *------------------------------------------------------------------------
- */
-void
-mmu_set_ttbr_XINU ( void )
-// mmu_set_ttbr ( void *base )
-{
-	unsigned int *base = xinu_page_table;
-
-	asm volatile (
-
-			/* We want to use TTBR0 only, 16KB page table */
-
-			"ldr	r0, =0x00000000\n"
-
-			/* Write the value into TTBCR */
-
-			"mcr	p15, 0, r0, c2, c0, 2\n"
-
-			/* Load the base address in r0 */
-
-			"mov	r0, %0\n"
-
-			/* Set table walk attributes */
-
-			"orr	r0, #0x00000002\n" /* shareable cacheable */
-			"orr	r0, #0x00000008\n" /* outer write-back write-allocate */
-			"orr	r0, #0x00000001\n" /* inner write-back write-allocate */
-
-			/* Write the new TTBR0 */
-
-			"mcr	p15, 0, r0, c2, c0, 0\n"
-
-			/* Write the new TTBR1 (just in case) */
-
-			"mcr	p15, 0, r0, c2, c0, 1\n"
-
-			/* Perform memory synchronization */
-			"isb\n"
-			"dsb\n"
-			"dmb\n"
-
-			:		/* Output	*/
-			: "r" (base)	/* Input	*/
-			: "r0"		/* Clobber	*/
-		);
-}
+#endif
 
 /* THE END */
