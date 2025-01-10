b556b6b11d72fd0bbce54d16900ac422b3351588
Tom Trebisky <tom@mmto.org>
2025-01-09 22:11:21 -0700

    Add test code for hub/emio

--- zynq/hub75.c
+++ zynq/hub75.c
@@ -4,11 +4,20 @@
  */
 
 // First pin is J5-5 == T16 == emio16
-#define TEST_PIN	16
+// #define TEST_PIN	16
+
+// J5-15
+// #define TEST_PIN	17
+// J5-12
+// #define TEST_PIN	18
+// J5-11
+#define TEST_PIN	19
 
 static int hub_pin;
 static int hub_state = 0;
 
+/* Generate 100 Hz square wave on whatever pin is selected
+ */
 static void
 hub_wave ( int xxx )
 {
@@ -21,6 +30,33 @@ hub_wave ( int xxx )
 		}
 }
 
+static void
+hub_tester ( void )
+{
+		char buf[64];
+		int nn;
+
+		printf ( "Testing on pin %d\n", hub_pin );
+
+		for ( ;; ) {
+			printf ( "HUB, type number for emio: " );
+			getline ( buf, 64 );
+			if ( ! buf[0] )
+				continue;
+			if ( buf[0] == 'q' )
+				break;
+			nn = atoi ( buf );
+			printf ( "You asked for %d\n", nn );
+			if ( nn < 5 || nn > 47 ) {
+				printf ( "Out of range\n" );
+				continue;
+			}
+			emio_config_output ( nn );
+			hub_pin = nn;
+			printf ( "Testing on pin %d\n", hub_pin );
+		}
+}
+
 void
 hub_test ( void )
 {
@@ -30,6 +66,8 @@ hub_test ( void )
 
 		/* run at 1000/5 = 200 Hz */
 		(void) thr_new_repeat ( "hub", hub_wave, 0, 25, 0, 5 );
+
+		hub_tester ();
 }
 
 /* THE END */
